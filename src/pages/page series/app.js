bc75ba7f0ccadb01a11534d8623db832c53b0ce9
Erika <146768635+erigt@users.noreply.github.com>
2023-12-10 21:30:32 +0100

    changes to the dev branch

---
+++ src/pages/page series/app.js
@@ -0,0 +1,15 @@
+var items = $('.card-content-red .card-red-products');
+            var numItems = items.length;
+            var perPage = 6;
+            items.slice(perPage).hide();
+            $('#pagination-container').pagination({
+                items: numItems,
+                itemsOnPage: perPage,
+                prevText: "<",
+                nextText: ">",
+                onPageClick: function (pageNumber) {
+                    var showFrom = perPage * (pageNumber - 1);
+                    var showTo = showFrom + perPage;
+                    items.hide().slice(showFrom, showTo).show();
+                }
+            })
